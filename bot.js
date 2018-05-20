c412daf084ff5e2aa4ee36d23b25ab3c65753ac5
Shaun <shaunidiot@users.noreply.github.com>
2018-05-20 13:16:58 +0800

    Add updateRepository method

--- bot.js
+++ bot.js
@@ -2,13 +2,39 @@ const fs = require('fs-extra');
 const config = require('./config');
 const snoowrap = require('snoowrap');
 const utils = require('./utils');
+const request = require('request');
+const rp = require('request-promise');
+
+const FDROID_REPO_XML = "https://f-droid.org/repo/index.xml";
+const REPO_FILENAME = 'repo.xml';
+
 class RedditBot {
     constructor() {
+        this.repoFileDirectory = `${__dirname}/${REPO_FILENAME}`;
+        this.repoFile = null;
         this.botRunningFile = `${__dirname}/${config.botRunningFile}`;
         this.reddit = new snoowrap(config.account);
         if (typeof this.reddit.getMe() == 'undefined') {
             console.error('Unable to get bot info..');
             return process.exit(1);
+        }
+    }
+
+    updateRepository(callback) {
+        if (!fs.pathExistsSync(this.repoFileDirectory) || ((parseInt(fs.statSync(__dirname + '/config.js').mtimeMs / 1000) + 86400) < Math.floor(new Date() / 1000))) {
+            console.log('Downloading repository..');
+            rp(FDROID_REPO_XML)
+                .then(htmlString => {
+                    fs.writeFileSync(this.repoFileDirectory, htmlString);
+                    console.log('Downloaded repository..');
+                    return callback();
+                })
+                .catch(error => {
+                    return callback(error);
+                });
+        } else {
+            console.log('Repository is up-to-date..');
+            return callback();
         }
     }
 
@@ -40,7 +66,6 @@ class RedditBot {
         console.debug('Bot logging in..');
         this.reddit.getSubreddit(config.subreddits.join('+')).getNewComments().map(comment => {
             let converted = utils.convertMarkdown(comment.body);
-            // console.log(comment.body);
             var commentRegex = /foss[\s]*me[\s]*:[\s]*(.*?)(?:\.|;|$)/gim;
             var match = commentRegex.exec(converted);
             if (match != null) {
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const fastXmlParser = require('fast-xml-parser');
-const snoowrap = require('snoowrap');
-const request = require('request');
-const config = require('./config');
-// const jsonObj = fastXmlParser.convertToJson(tObj, options);
-
-const FDROID_REPO_XML = "https://f-droid.org/repo/index.xml";
-
 const RedditBot = require('./bot');
-RedditBot.start();
+RedditBot.updateRepository(error => {
+    if (error) {
+        return console.log(error);
+    }
+
+    RedditBot.start();
+});
