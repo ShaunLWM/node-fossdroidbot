ecf0e7c3306741b1af1e05d2ec24a44fe62b4264
shaunidiot <shaunblacksheep@gmail.com>
2018-05-23 22:05:44 +0800

    Revoke accidental commit of token

---
+++ config.sample.js
@@ -0,0 +1,14 @@
+module.exports = {
+    account: {
+        userAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7',
+        clientId: '', // revoked. use your own
+        clientSecret: '', // revoked. use your own
+        refreshToken: '' // revoked. use your own
+    },
+    subreddits: ['test'],
+    maxAppsPerComment: 10,
+    closingFormula: "\n\n------\n\n[^Source ^Code](https://github.com/crisbal/PlayStoreLinks_Bot) ^| [^Feedback/Bug ^report](http://www.reddit.com/r/cris9696) ^| ^Bot ^by ^/u/cris9696 ",
+    logFile: "bot.log",
+    botRunningFile: "botRunning",
+    nosqlFile: 'database.nosql'
+}
